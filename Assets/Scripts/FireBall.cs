29ea509ee01d17dd3ead750a0c09bd9cb0eca58b
akshay-yewale <akyyewale@gmail.com>
2019-01-25 00:50:57 -0600

    FireScene actual implementation of mechanics. open Fire Scene, Click on play and press SPACE BAR.

---
+++ Assets/Scripts/FireBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public float Speedmultiplier = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject gameObject = Instantiate<GameObject>(BulletPrefab, this.transform.position,Quaternion.identity);
+            gameObject.transform.forward = this.transform.forward;
+            gameObject.GetComponent<Rigidbody>().AddForce(BulletPrefab.transform.forward * Speedmultiplier);
+            Debug.Log(BulletPrefab.transform.forward * Speedmultiplier);
+            Destroy(gameObject, 2.0f);
+        }
+    }
+}

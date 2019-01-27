0f2a5be5a25541c48fb4e96472de1e1fb992adf7
akshay-yewale <akyyewale@gmail.com>
2019-01-27 17:47:49 -0600

    Added Physic Materials and other scripts

--- Assets/Prefabs/Sphere.cs
+++ Assets/Prefabs/Sphere.cs
@@ -7,9 +7,12 @@ public class Sphere : MonoBehaviour
 
     private ParticleSystem system;
 
+    [SerializeField]
+    private GameObject effect;
+
     private void Start()
     {
-        if(this.gameObject.GetComponent<Rigidbody>() == null)
+        if (this.gameObject.GetComponent<Rigidbody>() == null)
         {
             this.gameObject.AddComponent<Rigidbody>();
         }
@@ -19,30 +22,10 @@ public class Sphere : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("Trigger hit");
-            system = this.GetComponent<ParticleSystem>();
-            // A simple particle material with no texture.
-            Material particleMaterial = new Material(Shader.Find("Particles/Standard Unlit"));
-
-            // Create a particle system.
-            var go = new GameObject("Particle System");
-            go.transform.Rotate(-90, 0, 0); // Rotate so the system emits upwards.
-            system = go.AddComponent<ParticleSystem>();
-            go.GetComponent<ParticleSystemRenderer>().material = particleMaterial;
-            Destroy(go, 3.0f);
-            // Every 2 seconds we will emit.
-            Invoke("DoEmit", 0.1f);
-
+            GameObject gameObject = Instantiate(effect, collision.GetContact(0).point, Quaternion.identity);
+            gameObject.transform.Rotate(-90.0f, 0.0f, 0.0f);
+            Destroy(gameObject, 3.0f);
         }
-    }
 
-    void DoEmit()
-    {
-        // Any parameters we assign in emitParams will override the current system's when we call Emit.
-        // Here we will override the position and velocity. All other parameters will use the behavior defined in the Inspector.
-        var emitParams = new ParticleSystem.EmitParams();
-        emitParams.position = new Vector3(0.0f, 0.0f, 0.0f);
-        emitParams.velocity = new Vector3(0.0f, 0.0f, -2.0f);
-        system.Emit(emitParams, 1);
     }
 }
--- Assets/Scripts/FireBall.cs
+++ Assets/Scripts/FireBall.cs
@@ -21,7 +21,7 @@ public class FireBall : MonoBehaviour
             GameObject gameObject = Instantiate<GameObject>(BulletPrefab, this.transform.position,Quaternion.identity);
             gameObject.transform.forward = this.transform.forward;
             gameObject.GetComponent<Rigidbody>().AddForce(BulletPrefab.transform.forward * Speedmultiplier);
-            Debug.Log(BulletPrefab.transform.forward * Speedmultiplier);
+            
             Destroy(gameObject, 2.0f);
         }
     }
